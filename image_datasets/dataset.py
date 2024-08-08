b1e524f460097840c2560716455dd81ebcd60eae
Brandon G. Neri <arcaniteamp@gmail.com>
2024-08-08 17:27:29 -0600

    fix image loader

--- image_datasets/dataset.py
+++ image_datasets/dataset.py
@@ -7,6 +7,7 @@ from torch.utils.data import Dataset, DataLoader
 import json
 import random
 
+
 def c_crop(image):
     width, height = image.size
     new_size = min(width, height)
@@ -16,9 +17,14 @@ def c_crop(image):
     bottom = (height + new_size) / 2
     return image.crop((left, top, right, bottom))
 
+
 class CustomImageDataset(Dataset):
     def __init__(self, img_dir, img_size=512):
-        self.images = [os.path.join(img_dir, i) for i in os.listdir(img_dir) if '.jpg' in i or '.png' in i]
+        self.images = [
+            os.path.join(img_dir, i)
+            for i in os.listdir(img_dir)
+            if ".jpg" in i or ".png" in i
+        ]
         self.images.sort()
         self.img_size = img_size
 
@@ -27,13 +33,15 @@ class CustomImageDataset(Dataset):
 
     def __getitem__(self, idx):
         try:
-            img = Image.open(self.images[idx])
+            img = Image.open(self.images[idx]).convert(
+                "RGB"
+            )  # Ensure image is in RGB format
             img = c_crop(img)
             img = img.resize((self.img_size, self.img_size))
             img = torch.from_numpy((np.array(img) / 127.5) - 1)
             img = img.permute(2, 0, 1)
-            json_path = self.images[idx].split('.')[0] + '.json'
-            prompt = json.load(open(json_path))['caption']
+            json_path = self.images[idx].split(".")[0] + ".json"
+            prompt = json.load(open(json_path))["caption"]
             return img, prompt
         except Exception as e:
             print(e)
@@ -42,4 +50,6 @@ class CustomImageDataset(Dataset):
 
 def loader(train_batch_size, num_workers, **args):
     dataset = CustomImageDataset(**args)
-    return DataLoader(dataset, batch_size=train_batch_size, num_workers=num_workers, shuffle=True)
+    return DataLoader(
+        dataset, batch_size=train_batch_size, num_workers=num_workers, shuffle=True
+    )
